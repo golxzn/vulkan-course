b6cbdfecceaa876e73464ee3496501eb101ac1d8
Ruslan Golovinskii <golxzn@gmail.com>
2024-08-30 09:28:36 +0400

    include glfw to the main target directly to fix build & make demo

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,26 @@
 #include <cstdio>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
 int main() {
-	std::printf("hi\n");
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	auto window{ glfwCreateWindow(1024, 720, "Vulkan Course", nullptr, nullptr) };
+
+	uint32_t extensions_count{};
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensions_count, nullptr);
+
+	std::printf("Vulkan extensions count: %u", extensions_count);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(std::exchange(window, nullptr));
+	glfwTerminate();
 }

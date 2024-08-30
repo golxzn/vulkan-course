a30634f54dc0e3626869253207a8048a93decbd5
Ruslan Golovinsky <ruslan.golovinsky@mytona.com>
2024-08-30 08:48:22 +0400

    initial commit

---
+++ cmake/config.cmake
@@ -0,0 +1,10 @@
+
+#=========================== OPTIONS ============================#
+
+set(ALIAS_PREFIX vc)
+
+#========================= CXX STANDARD =========================#
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
---
+++ code/main.cpp
@@ -0,0 +1,5 @@
+#include <cstdio>
+
+int main() {
+	std::printf("hi\n");
+}
